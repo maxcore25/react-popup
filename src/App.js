3ff8bea3b79d6175a7fdce860e311c1841de0d29
maxcore25 <mkorobov2002@mail.ru>
2022-01-12 23:48:38 +0300

    Initial Styles removed

--- src/App.js
+++ src/App.js
@@ -3,22 +3,65 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <section className='hero-section'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Do so written as raising parlors spirits mr elderly. Made late in of
+          high left hold. Carried females of up highest calling. Limits marked
+          led silent dining her she far. Sir but elegance marriage dwelling
+          likewise position old pleasure men. Dissimilar themselves simplicity
+          no of contrasted as. Delay great day hours men. Stuff front to do
+          allow to asked he. Name were we at hope. Remainder household direction
+          zealously the unwilling bed sex. Lose and gay ham sake met that. Stood
+          her place one ten spoke yet. Head case knew ever set why over.
+          Marianne returned of peculiar replying in moderate. Roused get enable
+          garret estate old county. Entreaties you devonshire law dissimilar
+          terminated. Luckily friends do ashamed to do suppose. Tried meant mr
+          smile so. Exquisite behaviour as to middleton perfectly. Chicken no
+          wishing waiting am. Say concerns dwelling graceful six humoured.
+          Whether mr up savings talking an. Active mutual nor father mother
+          exeter change six did all. Frankness applauded by supported ye
+          household. Collected favourite now for for and rapturous repulsive
+          consulted. An seems green be wrote again. She add what own only like.
+          Tolerably we as extremity exquisite do commanded. Doubtful offended do
+          entrance of landlord moreover is mistress in. Nay was appear entire
+          ladies. Sportsman do allowance is september shameless am sincerity oh
+          recommend. Gate tell man day that who. Tiled say decay spoil now walls
+          meant house. My mr interest thoughts screened of outweigh removing.
+          Evening society musical besides inhabit ye my. Lose hill well up will
+          he over on. Increasing sufficient everything men him admiration
+          unpleasing sex. Around really his use uneasy longer him man. His our
+          pulled nature elinor talked now for excuse result. Admitted add
+          peculiar get joy doubtful. Questions explained agreeable preferred
+          strangers too him her son. Set put shyness offices his females him
+          distant. Improve has message besides shy himself cheered however how
+          son. Quick judge other leave ask first chief her. Indeed or remark
+          always silent seemed narrow be. Instantly can suffering pretended
+          neglected preferred man delivered. Perhaps fertile brandon do imagine
+          to cordial cottage. Carriage quitting securing be appetite it
+          declared. High eyes kept so busy feel call in. Would day nor ask walls
+          known. But preserved advantage are but and certainty earnestly
+          enjoyment. Passage weather as up am exposed. And natural related man
+          subject. Eagerness get situation his was delighted. View fine me gone
+          this name an rank. Compact greater and demands mrs the parlors. Park
+          be fine easy am size away. Him and fine bred knew. At of hardly sister
+          favour. As society explain country raising weather of. Sentiments nor
+          everything off out uncommonly partiality bed. Adieus except say barton
+          put feebly favour him. Entreaties unpleasant sufficient few pianoforte
+          discovered uncommonly ask. Morning cousins amongst in mr weather do
+          neither. Warmth object matter course active law spring six. Pursuit
+          showing tedious unknown winding see had man add. And park eyes too
+          more him. Simple excuse active had son wholly coming number add.
+          Though all excuse ladies rather regard assure yet. If feelings so
+          prospect no as raptures quitting. Far quitting dwelling graceful the
+          likewise received building. An fact so to that show am shed sold cold.
+          Unaffected remarkably get yet introduced excellence terminated led.
+          Result either design saw she esteem and. On ashamed no inhabit ferrars
+          it ye besides resolve. Own judgment directly few trifling. Elderly as
+          pursuit at regular do parlors. Rank what has into fond she.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </section>
+    </>
   );
 }
 
